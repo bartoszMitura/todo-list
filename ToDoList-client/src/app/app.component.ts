28353abd3be3dff67d73ebd4b5f9871c81411aeb
Bartosz Mitura <bartosz.mitura@comarch.com>
2025-06-25 22:03:45 +0200

    feat: Implement Kanban board with drag-and-drop functionality; enhance task management and UI

--- ToDoList-client/src/app/app.component.ts
+++ ToDoList-client/src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
-import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet, RouterLink, RouterLinkActive, Router, NavigationEnd } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { MaterialModule } from './material.module';
 import { CommonModule } from '@angular/common';
 import { AuthService } from './services/auth.service';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -19,10 +20,23 @@ import { AuthService } from './services/auth.service';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ToDoList-client';
+  isKanbanView = false;
   
-  constructor(public authService: AuthService) {}
+  constructor(
+    public authService: AuthService,
+    private router: Router
+  ) {}
+  
+  ngOnInit() {
+    // Track router events to detect when we're on the Kanban view
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: any) => {
+      this.isKanbanView = event.url === '/kanban';
+    });
+  }
   
   get isLoggedIn(): boolean {
     return this.authService.isLoggedIn;
